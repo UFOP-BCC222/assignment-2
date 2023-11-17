08ac64e4238c57da54ceade0472d08f88f822015
Niege <127705690+niegereis@users.noreply.github.com>
2023-11-17 14:38:00 -0300

    Update Valor.hs

--- src/Valor.hs
+++ src/Valor.hs
@@ -1,5 +1,12 @@
 module Valor where
 
 valorTotal :: Double -> Double -> Double
-valorTotal morangos maçãs
-  = undefined
+valorTotal morangos maçãs = valorMorangos + valorMacas
+  where
+    valorMorangos
+      | morangos > 5 = morangos * 7.20
+      | morangos <= 5 = morangos * 8.5
+
+    valorMacas
+      | maçãs > 5 = maçãs * 4.75
+      | maçãs <= 5 = maçãs * 5.25
