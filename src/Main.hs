6dae022dd4721a4df5e65b2e012e80c6005b5ead
Niege <127705690+niegereis@users.noreply.github.com>
2023-11-17 14:37:09 -0300

    Update Main.hs

--- src/Main.hs
+++ src/Main.hs
@@ -5,5 +5,15 @@ import Valor (valorTotal)
 import System.IO (stdout, hSetBuffering, BufferMode(NoBuffering))
 
 main :: IO ()
-main =
-  undefined
+main = do
+  hSetBuffering stdout NoBuffering
+  putStrLn "Digite a quantidade de Morangos(em kg): "
+  morangos <- readLn
+  putStrLn "Digite a quantidade de Maçãs(em kg): "
+  macas <- readLn
+  if macas < 0 || morangos < 0
+    then putStrLn "Entrada inválida"
+    else do
+      let valor = valorTotal morangos macas
+      putStrLn ("O valor total da compra é R$ " ++ show valor)
+  
